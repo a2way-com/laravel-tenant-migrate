dfe32f610cdd96a7ae7af59eea154bb0ffdeac6d
Supun Budhajeewa <budhajeewa@gmail.com>
2014-11-08 15:45:02 +0530

    Initial Commit

---
+++ src/A2way/LaravelTenantMigrate/LaravelTenantMigrateServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace A2way\LaravelTenantMigrate;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTenantMigrateServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('a2way/laravel-tenant-migrate');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
