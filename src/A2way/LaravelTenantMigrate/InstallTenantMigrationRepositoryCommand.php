f0ebf6e2c782a715cf955afee7a93077c11fc45e
Christian Nielsen <cn@ordrestyring.dk>
2019-01-30 10:55:28 +0100

    laravel 5.5 upgrade

--- src/A2way/LaravelTenantMigrate/InstallTenantMigrationRepositoryCommand.php
+++ src/A2way/LaravelTenantMigrate/InstallTenantMigrationRepositoryCommand.php
@@ -1,78 +1,77 @@
 <?php
 namespace A2way\LaravelTenantMigrate;
 
-use \Illuminate\Console\Command as Command;
-use \Symfony\Component\Console\Input\InputOption as InputOption;
-use \Symfony\Component\Console\Input\InputArgument as InputArgument;
+use Illuminate\Console\Command as Command;
+use Symfony\Component\Console\Input\InputArgument as InputArgument;
+use Symfony\Component\Console\Input\InputOption as InputOption;
 
-class InstallTenantMigrationRepositoryCommand extends Command {
+class InstallTenantMigrationRepositoryCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'migrate:tenant:install';
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'migrate:tenant:install';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '"migrate" tenant database.';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = '"migrate" tenant database.';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $connectionName = $this->argument('connection-name');
+        $databaseName = $this->argument('database-name');
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
-		$connectionName = $this->argument('connection-name');
-		$databaseName = $this->argument('database-name');
-		
-		\Config::set('database.connections.'.$connectionName.'.database', $databaseName);
-		$connection = \DB::reconnect($connectionName);
-		\DB::setDefaultConnection($connectionName);
+        \Config::set('database.connections.' . $connectionName . '.database', $databaseName);
+        $connection = \DB::reconnect($connectionName);
+        \DB::setDefaultConnection($connectionName);
 
-		$this->info('Creating migration table in tenant database "'.$databaseName.'"...');
+        $this->info('Creating migration table in tenant database "' . $databaseName . '"...');
 
-		$this->call('migrate:install');
-	}
+        $this->call('migrate:install');
+    }
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return array(
-			array('connection-name', InputArgument::REQUIRED, 'Tenant connection name.'),
-			array('database-name', InputArgument::REQUIRED, 'Tenant database name.'),
-		);
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['connection-name', InputArgument::REQUIRED, 'Tenant connection name.'],
+            ['database-name', InputArgument::REQUIRED, 'Tenant database name.'],
+        ];
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array(
-		//	array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-		);
-	}
-
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            //	array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+        ];
+    }
 }
